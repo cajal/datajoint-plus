87f226aaa032eb093294a45d2fb2571b1cf1b144
spapa013 <stelios@spapa.us>
2022-03-01 08:05:37 -0600

    formatting, add 'free_table'

--- datajoint_plus/__init__.py
+++ datajoint_plus/__init__.py
@@ -5,7 +5,7 @@ master-part relationship, providing templates for commonly used motifs.
 """
 
 __all__ = ['__version__',
-           'DataJointPlusModule', 'create_djp_module',
+           'free_table', 'DataJointPlusModule', 'create_djp_module',
            'reassign_master_attribute', 'add_datajoint_plus',
            'Manual', 'Lookup', 'Imported', 'Computed', 'Part',
            'add_objects', 'check_if_latest_version', 'enable_datajoint_flags',
@@ -13,14 +13,17 @@ __all__ = ['__version__',
            'generate_hash', 'validate_and_generate_hash', 'parse_definition', 
            'reform_definition']
 
+from datajoint import FreeTable as free_table
+
 from .compatibility import (DataJointPlusModule, add_datajoint_plus,
                             reassign_master_attribute)
 from .hash import generate_hash, validate_and_generate_hash
-from .user_tables import Computed, Imported, Lookup, Manual, Part
-from .utils import (add_objects, check_if_latest_version, enable_datajoint_flags,
-                    format_table_name, make_store_dict, register_externals)
-from .version import __version__
 from .heading import parse_definition, reform_definition
+from .user_tables import Computed, Imported, Lookup, Manual, Part
+from .utils import (add_objects, check_if_latest_version,
+                    enable_datajoint_flags, format_table_name, make_store_dict,
+                    register_externals)
+from .version import __version__
 
 create_djp_module = DataJointPlusModule # Aliases for DataJointPlusModule
 
