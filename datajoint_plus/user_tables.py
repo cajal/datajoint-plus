4e2f3cd7ce6e4439a6233cbf1a3c3002a096e1ba
spapa013 <stelios@spapa.us>
2022-03-01 08:08:14 -0600

    modify UserTable to update Tables upon declare and drop_quick

--- datajoint_plus/user_tables.py
+++ datajoint_plus/user_tables.py
@@ -2,25 +2,44 @@
 Hosts the original DataJoint table tiers extended with DataJointPlus.
 """
 import datajoint as dj
+from datajoint.user_tables import UserTable
 
 from .base import MasterBase, PartBase
+from .table import TableLog
 
 
-class Lookup(MasterBase, dj.Lookup):
+class UserTable(UserTable):
+    def declare(self, context=None):
+        super().declare(context=context)
+        self._tables(self.table_id, self.full_table_name, action='add')
+    
+    def drop_quick(self):
+        super().drop_quick()
+        self._tables(self.table_id, action='delete')
+
+    @property
+    def _tables(self):
+        if self._tables_ is None:
+            self._tables_ = TableLog(self.connection, database=self.database)
+        return self._tables_
+
+    
+
+class Lookup(MasterBase, UserTable, dj.Lookup):
     pass
 
 
-class Manual(MasterBase, dj.Manual):
+class Manual(MasterBase, UserTable, dj.Manual):
     pass
 
 
-class Computed(MasterBase, dj.Computed):
+class Computed(MasterBase, UserTable, dj.Computed):
     pass
 
 
-class Imported(MasterBase, dj.Imported):
+class Imported(MasterBase, UserTable, dj.Imported):
     pass
 
 
-class Part(PartBase, dj.Part):
+class Part(PartBase, UserTable, dj.Part):
     pass
