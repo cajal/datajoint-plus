d559ac10cde31a5a2c067bfa40b18c39733a0754
spapa013 <stelios@spapa.us>
2022-03-10 11:55:12 -0600

    adds UserTable to user_tables.py

--- datajoint_plus/user_tables.py
+++ datajoint_plus/user_tables.py
@@ -4,53 +4,19 @@ Hosts the original DataJoint table tiers extended with DataJointPlus.
 import re
 
 import datajoint as dj
-from datajoint.user_tables import UserTable
-
-from .base import MasterBase, PartBase
-from .table import Tables
-from .utils import classproperty, goto
+from .table import Table
+from .base import BaseMaster, BasePart
 
 master_classes = (dj.Manual, dj.Lookup, dj.Computed, dj.Imported,)
 part_classes = (dj.Part,)
 
-class UserTable(UserTable):
-    """
-    Extensions to DataJoint UserTable
-    """
-    
-    _tables_ = None
 
-    def declare(self, context=None):
-        super().declare(context=context)
-        self._tables(self.table_id, self.full_table_name, action='add')
-    
-    def drop(self):
-        super().drop()
-        self._tables(self.table_id, action='delete')
-
-    def drop_quick(self):
-        super().drop_quick()
-        self._tables(self.table_id, action='delete')
-
-    def goto(self, table_id=None, attr=None, directory='current_module'):
-        if table_id is None:
-            assert attr is not None, 'If table_id is not provided, provide attr in self that hosts table_id'
-            return goto(self.fetch1(attr), directory=directory)
-        else:
-            return goto(table_id, directory=directory)
-
-
+class UserTable(Table, dj.user_tables.UserTable):
     @property
-    def _tables(self):
-        if self._tables_ is None:
-            self._tables_ = Tables(self.connection, database=self.database)
-        return self._tables_
-
-    @classproperty
-    def is_user_table(cls):
+    def is_user_table(self):
         return True
 
-    @classproperty
+    @classmethod
     def is_master(cls):
         try:
             next(tier for tier in master_classes
@@ -59,7 +25,7 @@ class UserTable(UserTable):
         except StopIteration:
             return False
 
-    @classproperty
+    @classmethod
     def is_part(cls):
         try:
             next(tier for tier in part_classes
@@ -67,23 +33,41 @@ class UserTable(UserTable):
             return True
         except StopIteration:
             return False
+
+
+class Lookup(BaseMaster, UserTable, dj.Lookup):
+    def __init_subclass__(cls, **kwargs):
+        cls._init_validation(**kwargs)
+
+
+class Manual(BaseMaster, UserTable, dj.Manual):
+    def __init_subclass__(cls, **kwargs):
+        cls._init_validation(**kwargs)
+
+
+class Computed(BaseMaster, UserTable, dj.Computed):
+    def __init_subclass__(cls, **kwargs):
+        cls._init_validation(**kwargs)
+
+
+class Imported(BaseMaster, UserTable, dj.Imported):
+    def __init_subclass__(cls, **kwargs):
+        cls._init_validation(**kwargs)
+
+
+class Part(BasePart, UserTable, dj.Part):
     
+    def __init_subclass__(cls, **kwargs):
+        cls._init_validation(**kwargs)
 
-class Lookup(MasterBase, UserTable, dj.Lookup):
-    pass
+    def drop(self, force=False):
+        if force:
+            super(UserTable, self).drop()
+        else:
+            raise dj.DataJointError('Cannot drop a Part directly.  Delete from master instead')
 
-
-class Manual(MasterBase, UserTable, dj.Manual):
-    pass
-
-
-class Computed(MasterBase, UserTable, dj.Computed):
-    pass
-
-
-class Imported(MasterBase, UserTable, dj.Imported):
-    pass
-
-
-class Part(PartBase, UserTable, dj.Part):
-    pass
+    def drop_quick(self, force=False):
+        if force:
+            return super(UserTable, self).drop_quick()
+        else:
+            raise dj.DataJointError('Cannot drop a Part directly.  Delete from master instead')
