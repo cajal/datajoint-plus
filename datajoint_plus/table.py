64b33eec318a971cdd0e8a2facd5db8eef5b83ba
spapa013 <stelios@spapa.us>
2022-03-10 11:54:58 -0600

    adds Table and Tables

--- datajoint_plus/table.py
+++ datajoint_plus/table.py
@@ -2,14 +2,126 @@
 Extensions of DataJoint Table
 """
 
-from copy import copy
 import logging
 
-from datajoint.table import Table
-from . import free_table, AndList
+import datajoint as dj
 from datajoint_plus.hash import generate_table_id
-
+from .utils import goto, wrap
 from .version import __version__ as version
+
+class Table(dj.table.Table):
+    """
+    Extensions to DataJoint Table
+    """
+    
+    _tables_ = None
+
+    def declare(self, context=None):
+        super().declare(context=context)
+        self._tables(self.table_id, self.full_table_name, action='add')
+    
+    def drop(self):
+        dj.table.Table.drop(self)
+        self._tables(self.table_id, action='delete')
+
+    def drop_quick(self):
+        dj.table.Table.drop_quick(self)
+        self._tables(self.table_id, action='delete')
+
+    def goto(self, table_id=None, full_table_name=None, tid_attr=None, ftn_attr=None, directory='__main__', return_free_table=False, ftn_lookup=None):
+        """
+        Extends datajoint_plus.utils.goto with the additional options:
+            :param tid_attr: (str) default=None, attribute in self that points to a table_id. Will be used as such: table_id = self.fetch1(tid_attr)
+            :param ftn_attr: (str) default=None, attribute in self that points to a full_table_name. Will be used as such: full_table_name = self.fetch1(ftn_attr)
+            :param return_free_table: (bool) Provides a FreeTable generated from full_table_name instead of using goto. 
+                If only table_id is known, ftn_lookup should provide a mapping between table_id and full_table_name. 
+            :param ftn_lookup: (DJ table) DataJoint table to query table_id and retrieve full_table_name if return_free_table is True
+                By default the following query will be attempted: (ftn_lookup & {'table_id': table_id}).fetch1('full_table_name')
+                If ftn_lookup uses a different attr name for table_id, provide it to tid_attr.
+        """
+        if isinstance(self, str):
+            raise AttributeError('Instantiate table to run goto().')
+        
+        if len(self)==1:
+            try:
+                table_id = self.fetch1('table_id')
+            except:
+                pass
+        
+        if table_id is None and tid_attr is not None:
+            table_id = self.fetch1(tid_attr)
+        
+        if full_table_name is None and ftn_attr is not None:
+            full_table_name = self.fetch1(ftn_attr)
+
+        if full_table_name is None and table_id is None:
+            raise Exception('Provide either table_id/full_table_name or tid_attr/ ftn_attr.')
+
+        if return_free_table:
+            if full_table_name is None:
+                try:
+                    if tid_attr is None:
+                        tid_attr = 'table_id'
+                    full_table_name = (ftn_lookup & {tid_attr: table_id}).fetch1('full_table_name')
+                except Exception as e:
+                    raise Exception('If return_free_table=True and full_table_name is not provided, table_id and ftn_lookup must be provided.')
+            return FreeTable(self.connection, full_table_name)
+        
+        else:
+            return goto(table_id=table_id, full_table_name=full_table_name, directory=directory)
+
+    def get(self, key=None, attrs=None):
+        """
+        Returns a single entry from table as a dictionary. 
+
+        Table must be restricted to a single entry before calling get(), or key to restrict table can be passed to get(). 
+        Optionally, attrs can be passed to return a subset of table attributes. 
+
+        :param key: (dict, QueryExpression, AndList, etc) a restriction for table
+        :param attrs: (str or list/ tuple) A single attr can be provided as a str, or a list/ tuple of strings
+
+        :returns: (dict) Dictionary containing fetch1 results
+        """
+        try:
+            if attrs is not None:
+                attrs = wrap(attrs)
+
+            if key is None:
+                if attrs is not None:
+                    result = self.fetch1(*attrs)
+                    result = wrap(result)
+                    return {a: r for a, r in zip(attrs, result)} # always return as dict
+                else:
+                    return self.fetch1()
+            else:
+                if attrs is not None:
+                    result = (self & key).fetch1(*attrs)
+                    result = wrap(result)
+                    return {a: r for a, r in zip(attrs, result)}
+                else:
+                    return (self & key).fetch1()
+        except AttributeError as e:
+            raise AttributeError(e.args[0] + f'. Did you instantiate the class?') from None
+
+    @property
+    def _tables(self):
+        if self._tables_ is None:
+            self._tables_ = Tables(self.connection, database=self.database)
+        return self._tables_
+
+
+class FreeTable(Table, dj.FreeTable):
+    """
+    Extension to DataJoint FreeTable
+    """
+    
+    def drop(self):
+        dj.table.Table.drop(self)
+        self._tables(full_table_name = self.full_table_name, action='delete')
+
+    def drop_quick(self):
+        dj.table.Table.drop_quick(self)
+        self._tables(full_table_name = self.full_table_name, action='delete')
 
 
 class Tables(Table):
@@ -54,7 +166,7 @@ class Tables(Table):
     def table_name(self):
         return '~tables'
 
-    def __call__(self, table_id=None, full_table_name=None, action=None):
+    def __call__(self, table_id=None, full_table_name=None, action=None, directory='__main__'):
         """
         :param table_id: unique ID of table to insert
         :param full_table_name: full table name (database + table)
@@ -90,7 +202,7 @@ class Tables(Table):
                 full_table_name_restr = {'full_table_name': full_table_name} if full_table_name is not None else None
                 
                 restr = [r for r in [table_id_restr, full_table_name_restr] if r is not None]
-                restr = self & AndList(restr)
+                restr = self & dj.AndList(restr)
 
                 if action == 'delete':
                     assert ~((table_id is None) and (full_table_name is None)), 'Provide table_id or full_table_name to delete.'
@@ -101,8 +213,9 @@ class Tables(Table):
                 if (table_id is None) and (full_table_name is None):
                     return self
 
-                full_table_name = restr.fetch1('full_table_name')
-                return free_table(self.connection, full_table_name)
+                table_id, full_table_name = restr.fetch1('table_id', 'full_table_name')
+                table = goto(table_id, directory=directory)
+                return table
 
         except Exception as e:
             logging.error(e)
@@ -111,12 +224,12 @@ class Tables(Table):
     @property
     def exists(self):
         """Returns existing tables"""
-        return self & {'exists': 1}
+        return FreeTable(self.connection, self.full_table_name) & dj.AndList([{'exists': 1}])
 
     @property
     def deleted(self):
         """Returns deleted tables"""
-        return self & {'exists': 0}
+        return FreeTable(self.connection, self.full_table_name) & dj.AndList([{'exists': 0}])
 
     def delete(self):
         """bypass interactive prompts and cascading dependencies"""
