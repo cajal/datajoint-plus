e27d7f681596435f0e0711e7b8944b2ee387ae56
spapa013 <stelios@spapa.us>
2022-03-28 13:38:56 -0500

    updates djp errors

--- datajoint_plus/errors.py
+++ datajoint_plus/errors.py
@@ -4,12 +4,35 @@ Exception classes for the DataJointPlus
 from datajoint.errors import DataJointError
 
 
-class ValidationError(DataJointError):
+class DataJointPlusError(DataJointError):
+    pass
+
+
+class ValidationError(DataJointPlusError):
     """
     Validation failure.
     """
 
-class OverwriteError(DataJointError):
+class OverwriteError(DataJointPlusError):
     """
     Overwrite not permissible.
     """
+
+class MotifError(DataJointPlusError):
+    pass
+
+
+class MakerError(MotifError):
+    pass
+
+
+class MakerInputError(MotifError):
+    pass
+
+
+class MakerMethodError(MotifError):
+    pass
+
+
+class MakerDestinationError(MotifError):
+    pass
