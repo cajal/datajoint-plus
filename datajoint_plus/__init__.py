c1d6ab463ec971c8ab4768f44f5fa28f3a88d4cb
spapa013 <stelios@spapa.us>
2022-05-09 03:02:22 -0500

    update log method and add util fxns

--- datajoint_plus/__init__.py
+++ datajoint_plus/__init__.py
@@ -16,9 +16,10 @@ __all__ = ['__version__',
            'add_objects', 'check_if_latest_version', 'enable_datajoint_flags',
            'format_table_name', 'split_full_table_name', 'make_store_dict', 'register_externals', 
            'generate_hash', 'validate_and_generate_hash', 'parse_definition', 
-           'reform_definition', 'errors', 'free_table',
-           'Entity', 'Method', 'Destination', 'EntityLookup', 'MethodLookup', 'StoreLookup', 'MakerLookup',
-           'NestedMaker', 'NestedStore', 'NestedMethod', 'configure_logger']
+           'reform_definition', 'errors', 'free_table', 
+           'basicConfig', 'getLogger', 'LogFileManager']
+
+
 
 # from DataJoint
 from datajoint.admin import kill, set_password
@@ -29,24 +30,21 @@ from datajoint.diagram import Diagram
 from datajoint.errors import DataJointError
 from datajoint.expression import AndList, Not, U
 from datajoint.fetch import key
-from datajoint.schemas import VirtualModule as DataJointVirtualModule
-from datajoint.schemas import list_schemas
-from datajoint.settings import config
+from datajoint.schemas import VirtualModule as DataJointVirtualModule, list_schemas 
 
 # from DataJointPlus
 from . import errors
 from .compatibility import add_datajoint_plus, reassign_master_attribute
+from .config import config
 from .hash import generate_hash, validate_and_generate_hash
 from .heading import parse_definition, reform_definition
-from .motif import (Entity, Method, Destination, 
-                                EntityLookup, MethodLookup, StoreLookup, MakerLookup,
-                                NestedMaker, NestedMethod, NestedStore)
+from .logging import LogFileManager, basicConfig, getLogger
 from .schema import DataJointPlusModule, Schema
 from .table import FreeTable as free_table
 from .user_tables import Computed, Lookup, Part
 from .utils import (add_objects, check_if_latest_version,
                     enable_datajoint_flags, format_table_name, make_store_dict,
-                    register_externals, split_full_table_name, configure_logger)
+                    register_externals, split_full_table_name)
 from .version import __version__
 
 # aliases
@@ -55,8 +53,6 @@ create_dj_virtual_module = DataJointVirtualModule
 schema = Schema 
 create_djp_module = DataJointPlusModule 
 
-# logger format
-configure_logger()
-
 # version control
 check_if_latest_version()
+
